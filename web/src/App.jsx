418f9894e37f773bfbad1a00a1eca8c8e031feb2
acs237 <acs23072000@gmail.com>
2025-12-13 16:07:15 +1100

    merge local commit to main

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SetupNotification from './components/SetupNotification';
 import ViewNotification from './components/ViewNotification';
+import GetNotification from './components/GetNotification';
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/noti/set" element={<SetupNotification />} />
         <Route path="/noti/view" element={<ViewNotification />} />
+        <Route path="/noti/get" element={<GetNotification />} />
       </Routes>
     </Router>
   )
