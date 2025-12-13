e5f32c13dfbeed8e743be2d58839f65bc897fba0
winnie <david.zhou@thetradedesk.com>
2025-12-13 18:34:27 +1100

    add job ui

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,6 +3,7 @@ import SetupNotification from './components/SetupNotification';
 import ViewNotification from './components/ViewNotification';
 import GetNotification from './components/GetNotification';
 import Dashboard from './components/Dashboard';
+import CreateJob from './components/CreateJob';
 import JobDetail from './components/JobDetail/JobDetail';
 import LandingPage from './pages/LandingPage';
 
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/jobs/new" element={<CreateJob />} />
         <Route path="/jobs/:id" element={<JobDetail />} />
         <Route path="/noti/set" element={<SetupNotification />} />
         <Route path="/noti/view" element={<ViewNotification />} />
